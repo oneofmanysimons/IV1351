fd7b4cf1be93d6c04b4305d65c2deaa3a3ef6903
oneofmanysimons <114488646+oneofmanysimons@users.noreply.github.com>
2023-12-19 16:51:59 +0100

    Add files via upload

--- query3.sql
+++ query3.sql
@@ -4,4 +4,5 @@ FULL JOIN lesson b ON a.instructor_id = b.instructor_id
 WHERE EXTRACT('month' FROM b.date) = EXTRACT('month' FROM NOW())
 AND EXTRACT('year' FROM b.date) = EXTRACT('year' FROM NOW())
 GROUP BY a.instructor_id
+HAVING COUNT(lesson_id) > 0
 ORDER BY number_of_lessons DESC;
